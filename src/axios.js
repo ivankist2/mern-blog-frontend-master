eb377e129132aa5c43e9f3311f8dd9e3d73330bc
Ivan Kist <ivankist2@email.com>
2024-07-10 17:04:37 +0300

    auth -> token

--- src/axios.js
+++ src/axios.js
@@ -6,4 +6,9 @@ const instance = axios.create({
     baseURL: `http://localhost:${PORT}`,
 });
 
+instance.interceptors.request.use((config) => {
+    config.headers.Authorization = window.localStorage.getItem('token');
+    return config;
+});
+
 export default instance;
